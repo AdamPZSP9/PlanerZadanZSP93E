94d2902699a3c74b29992474e59dbd3411a9c591
AdamPZSP <a.pabich@zsp9.elodz.edu.pl>
2026-04-29 10:26:59 +0200

    x

---
+++ src/navigation/topbar.jsx
@@ -0,0 +1,23 @@
+import "./topbar.css"
+
+function TopBar(){
+    <nav>
+        <table>
+            <tr>
+                <th><Link to="/styczen">Styczeń</Link></th>
+                <th><Link to="/luty">Luty</Link></th>
+                <th><Link to="/marzec">Marzec</Link></th>
+                <th><Link to="/kwiecien">Kwiecień</Link></th>
+                <th><Link to="/maj">Maj</Link></th>
+                <th><Link to="/czerwiec">Czerwiec</Link></th>
+                <th><Link to="/lipiec">Lipiec</Link></th>
+                <th><Link to="/sierpien">Sierpień</Link></th>
+                <th><Link to="/wrzesien">Wrzesień</Link></th>
+                <th><Link to="/pazdziernik">Październik</Link></th>
+                <th><Link to="/listopad">Listopad</Link></th>
+                <th><Link to="/grudzien">Grudzień</Link></th>
+            </tr>
+        </table>
+    </nav>
+}
+export default TopBar;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Link } from "react-router-dom";
+import TopBar from "../navigation/TopBar"
 function Home(){
     return(
     <>
+
         <nav>
             <Link to="/test">Wejdź na stronę testową</Link>
         </nav>
