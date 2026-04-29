3fdbec72eb11e8e839a3d4676a0886a2f4e578d9
KrystianS123 <k.skiba@zsp9.elodz.edu.pl>
2026-04-29 11:36:26 +0200

    zmiany w app css

--- src/navigation/TopBar/topbar.jsx
+++ src/navigation/TopBar/topbar.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 function TopBar(){
     return(
     <nav>
+        
         <table>
             <tr>
                 <th><Link to="/styczen">Styczeń</Link></th>
